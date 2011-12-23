e16951318415a07782fcaf847f8fd84f93652d0c
Limbo Peng <iwinux@gmail.com>
2011-12-23 01:04:23 -0500

    set make -j to use all cores

--- vim/setup.sh
+++ vim/setup.sh
@@ -8,7 +8,7 @@ wget $VIM_SOURCE_URL -O vim.tar.bz2
 tar xvf vim.tar.bz2
 cd $VIM_SOURCE_DIR
 ./configure --prefix=/usr --disable-darwin --disable-selinux --enable-pythoninterp=yes --disable-netbeans --enable-multibyte --disable-gtktest --disable-gpm --disable-nls --with-features=huge 
-make -j `cat /proc/cpuinfo | grep processor | wc -l`
+make -j NUM_OF_CORES
 make install
 
 [[ $? -eq 0 ]] && echo "Done setting up Vim"
