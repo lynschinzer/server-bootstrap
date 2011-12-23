ad34b65689c60d2eb1347c67bd9619cbb0715dca
Limbo Peng <iwinux@gmail.com>
2011-12-23 00:46:19 -0500

    boostrap script done

--- deps/setup.sh
+++ deps/setup.sh
@@ -2,6 +2,12 @@
 
 apt-get install --yes build-essential zlib1g zlib1g-dev libsqlite3-dev libssl-dev libreadline5-dev libxml2-dev libpcre3-dev libcurl4-openssl-dev libncurses5-dev curl htop tree openvpn
 
-apt-get remove --yes vim-tiny
+apt-get remove --yes vim-tiny vim
+
+if [[ $? -eq 0 ]]
+then
+    echo "Done setting up dependencies."
+    echo `date` > /var/cache/DEPS_INSTALLED
+fi
 
 exit $?
