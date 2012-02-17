751d711850d2acf2113795e8c5d5b92a9d0b12a5
Limbo Peng <iwinux@gmail.com>
2012-02-17 17:42:35 +0800

    fixed some typos

--- postgres/setup.sh
+++ postgres/setup.sh
@@ -5,7 +5,7 @@ POSTGRESQL_DEB_URL=http://oscg-downloads.s3.amazonaws.com/packages/postgres_9.1.
 POSTGRES_ROOT=/opt/postgres/9.1
 POSTGRES_DATA=/data/postgres/data
 
-if [[ -f $POSTGRES_DATA ]]
+if [[ -d $POSTGRES_DATA ]]
 then
     echo "PostgreSQL already installed and initialized!"
     exit 1
@@ -16,21 +16,23 @@ wget $POSTGRESQL_DEB_URL -O postgres.deb
 dpkg -i postgres.deb
 
 cd /etc/init.d
-mv postgres-9.1-openscg postgres
+rm postgres-9.1-openscg
+cp $CONFIG_DIR/postgres.init /etc/init.d/postgres
+chmod +x /etc/init.d/postgres
 update-rc.d postgres defaults
 
-mkdir -p $POSTGRES_DATA/pg_log
+mkdir -p $POSTGRES_DATA
 
 groupadd postgres
 useradd -m -c "PostgreSQL" -d $POSTGRES_DATA -g postgres postgres
 echo 'Setting password for user postgres...'
 passwd postgres
 
-chown -R postgres:postgres $POSTGRES_DATA
-
 su - postgres -c "LD_LIBRARY_PATH=$POSTGRES_ROOT/lib:$LD_LIBRARY_PATH $POSTGRES_ROOT/bin/initdb  -E utf8 -A md5 -D $POSTGRES_DATA -U postgres -W"
 
 cp $CONFIG_DIR/postgresql.conf $POSTGRES_DATA
 cp $CONFIG_DIR/bash_profile $POSTGRES_DATA/.bash_profile
+mkdir $POSTGRES_DATA/pg_log
+chown -R postgres:postgres $POSTGRES_DATA
 
 service postgres start
